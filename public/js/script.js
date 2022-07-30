42770c310511bbd8a829999f445c8e426cf26f56
ultexplorer <ultexplorer@gmail.com>
2022-07-30 14:47:08 +0300

    Added all files for project

---
+++ public/js/script.js
@@ -0,0 +1,50 @@
+let form = document.getElementById('form')
+if(form){
+    form.addEventListener('submit', function (ev){
+        if(this.dataset.id !== 'create'){
+            ev.preventDefault();
+
+            let data = {};
+
+            Array.from(this.getElementsByTagName('input')).forEach((el) => {
+                data[el.name] = el.value;
+            })
+
+            fetch(this.action, {
+                method: 'PUT',
+                body:JSON.stringify(data),
+                headers: {
+                    'Content-type': 'application/json;charset=utf-8'
+                }
+            })
+                .then(() => window.location.replace('/user'))
+                // .then((response) => {
+                //     return response.json()
+                // })
+                // .then(data => {
+                //     alert(data.message)
+                // })
+                .catch((err) => {
+                    alert('error')
+                })
+        }
+    })
+}
+
+Array.from(document.getElementsByClassName('del')).forEach(el => {
+    el.addEventListener('click', function (){
+        fetch('/user/' + this.dataset.id, {
+            method:'DELETE',
+        })
+            .then((response) => {
+                return response.json()
+            })
+            .then(data => {
+                this.parentNode.parentNode.remove()
+                //alert(data.message)
+            })
+            .catch((err) => {
+                alert('error')
+            })
+    })
+})
